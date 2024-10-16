31b6e996d0fded45f7da5a69a57e82054b5ae359
GhoffarFitassin <ghoffar8804@gmail.com>
2024-10-16 21:14:55 +0700

    add: Praktikum 2 (week 7)

---
+++ dasarweb/WEEK7/empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array();
+if (empty($myArray)) {
+    echo "Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array terdefinisikan dan tidak kosong.";
+}
+echo "<br>";
+if (empty($nonExistentVar)) {
+    echo "Array tidak terdefinisikan atau kosong.";
+} else {
+    echo "Array terdefinisikan dan tidak kosong.";
+}
+?>
