b305204c11042e32445b131a0b957eee32dfe399
GhoffarFitassin <ghoffar8804@gmail.com>
2024-10-21 10:27:42 +0700

    add: Praktikum 4 (week 8)

--- dasarweb/WEEK8/upload.js
+++ dasarweb/WEEK8/upload.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $('#upload-form').submit(function(e){
+    $("#file").change(function (e) {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+    $('#upload-form').submit(function (e) {
         e.preventDefault();
         var formData = new FormData(this);
         $.ajax({
